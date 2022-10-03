1adfdbe8fb5077cfe749d34f3d7da2c19db5446a
Juan J Guirao Garcia <juan.jose.guirao@gmail.com>
2022-10-03 08:29:41 +0100

    Fixed main layout. WOrking on form width

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,18 +3,20 @@ import './Message.css'
 
 function Message() {
     return (
-        <div className='message-wrapper'>
-            <h1>Message</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                eu sapien velit. Fusce vulputate porta vestibulum. Sed gravida
-                tellus sed nisi pulvinar, vel mollis nisi feugiat. Donec
-                fringilla dolor non metus tempor fermentum. Praesent
-                sollicitudin efficitur venenatis. Pellentesque laoreet, ipsum ut
-                fringilla feugiat, est erat facilisis erat, quis pharetra dui mi
-                nec sapien. Nunc lacinia nisl non pretium lacinia. Morbi mollis
-                a lorem non rhoncus.
-            </p>
+        <div className='message-container'>
+            <div className='message-wrapper'>
+                <h1>Message</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aenean eu sapien velit. Fusce vulputate porta vestibulum.
+                    Sed gravida tellus sed nisi pulvinar, vel mollis nisi
+                    feugiat. Donec fringilla dolor non metus tempor fermentum.
+                    Praesent sollicitudin efficitur venenatis. Pellentesque
+                    laoreet, ipsum ut fringilla feugiat, est erat facilisis
+                    erat, quis pharetra dui mi nec sapien. Nunc lacinia nisl non
+                    pretium lacinia. Morbi mollis a lorem non rhoncus.
+                </p>
+            </div>
         </div>
     )
 }
